45939bb9aec22b5f8d5ddd58d911a2bd3e56ae8b
Ivo Goncalves <ivojmgs19@gmail.com>
2023-03-29 16:08:57 -0300

    Exception for null objects

--- src/main/java/com/ivogoncalves/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/ivogoncalves/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ivogoncalves.exceptions.ExceptionResponse;
+import com.ivogoncalves.exceptions.RequiredObjectIsNullException;
 import com.ivogoncalves.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -27,5 +28,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex , WebRequest request){
 		ExceptionResponse exception = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(RequiredObjectIsNullException.class)
+	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse exception = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
 	}
 }
